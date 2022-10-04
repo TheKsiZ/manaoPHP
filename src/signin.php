77919103dbff5ef90e412d2cca49240e3eb19f8d
TheKsiZ <somethingksiz@gmail.com>
2022-10-04 16:21:54 +0300

    added "empty errors"

--- src/signin.php
+++ src/signin.php
@@ -1,3 +1,9 @@
+<?php    
+    session_start();
+    if(isset($_COOKIE["username"])){        
+        header("Location: ./index.php");        
+    }    
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,18 +11,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>    
     <title>Sign In</title>
 </head>
 <body>
+    <script src="./scripts/withscript.js"></script>
     <script src="./scripts/validation.js"></script>
     <div>
-        <form name="registration" action="" method="post"> <!-- No action -->
-            <input placeholder="Login" type="text" name="login" minlength="6" autofocus required> 
+        <form name="login" id="login" action="./scripts/login.php" method="post">
+            <input placeholder="Login" type="text" name="username" minlength="6" autofocus required> 
             <input placeholder="Password" type="password" id="password" name="password" minlength="6" required> 
-            <input type="submit" value="Submit" id="submit">
+            <span id="invalidError" value=""></span>
+            <input type="submit" value="Submit" id="submit" disabled>
         </form>
-        <a href="registration.php">Registration</a>
-    </div>
-    
+        <a href="./registration.php">Registration</a>
+    </div>    
 </body>
 </html>
